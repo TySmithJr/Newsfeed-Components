d08374d7b6f33770979a118c303e7c2c2a3ac910
Tyrone Smith <billionairefly.ts@gmail.com>
2019-05-08 17:53:35 -0400

    created a reference to the ".expandButton" class & updated the text on expandButton to say "expand"

--- Article/Article.js
+++ Article/Article.js
@@ -5,9 +5,11 @@ class Article {
     // assign this.domElement to the passed in domElement
     this.domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = this.domElement.querySelector(".expandButton");
+    this.closeButton = this.domElement.querySelector(".close");
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.textContent = "Expand";
+    this.closeButton.textContent = "Close";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     
   }
